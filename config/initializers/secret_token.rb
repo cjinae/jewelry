4db5bcacba1b9692870b6f821341e2d9e68a0fb3
Christine Lee <christine.jinae@gmail.com>
2013-06-06 21:53:20 -0400

    Initial Commit. Landing and Home page set us

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nineandknots::Application.config.secret_token = '63cd967ae2b60ab89ae9b08f99ae78442540d1806df838d88bb8b96afcb63727e9054540cd2be2d043fed74312f7c25f89249059639a7d6bd8e734396aac7c52'
