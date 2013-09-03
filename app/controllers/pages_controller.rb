f422da113e77beef9f9c09df4c7cc2171c55030e
Christine Lee <christine.jinae@gmail.com>
2013-09-03 11:34:23 -0400

    Adds pages controller for 'other' pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+
+	def show
+		# path = params[:url]  # this fist line is wronge
+		# layout = case path
+		# when 'campaign'
+		# 	'campaign'		
+		# end		
+		# render :pages => 'campaign'
+	end
+
+end
